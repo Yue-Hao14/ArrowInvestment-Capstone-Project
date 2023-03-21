0b41a4c7a717af0770a103b0b85d01fbb7c2a4b8
Yue Hao <105403119+Yue-Hao14@users.noreply.github.com>
2023-03-21 16:53:46 -0400

    stock seed data done and corrected stock_ticker from integer to varchar on db schema

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .watchlists import seed_watchlists, undo_watchlists
+from .stocks import seed_stocks,undo_stocks
 
 from app.models.db import db, environment, SCHEMA
 
@@ -17,16 +18,19 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_stocks()
         undo_watchlists()
         undo_users()
     seed_users()
     seed_watchlists()
+    seed_stocks()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_stocks()
     undo_watchlists()
     undo_users()
     # Add other undo functions here
