9841d9faa2ec9eaf2910edd92fe03e94be2ca959
Yue Hao <105403119+Yue-Hao14@users.noreply.github.com>
2023-03-29 12:03:14 -0400

    create a helper function to calculate DoD and DoDPercChange

--- react-app/src/components/Watchlists/WatchlistDetailsSection.js
+++ react-app/src/components/Watchlists/WatchlistDetailsSection.js
@@ -1,9 +1,14 @@
 import { NavLink } from "react-router-dom";
 import OneDayChart from "../StockPage/OneDayChart";
+import { stockOneDayChange } from "../../utils/FetchStockData";
 import './WatchlistDetailsSection.css'
+import { useEffect } from "react";
 
 function WatchlistDetails({ watchlist }) {
   const stocksArr = watchlist.stocks
+  useEffect(() => {
+
+  })
   // console.log(stocksArr)
   return (
     <div className="watchlist-details-container">
@@ -11,8 +16,8 @@ function WatchlistDetails({ watchlist }) {
         <NavLink to={`/stocks/${stock.ticker}`} className="watchlist-details-individual-container" key={stock.ticker}>
           <div className="watchlist-details-ticker">{stock.ticker}</div>
           <div className="watchlist-details-chart-container"><OneDayChart ticker={stock.ticker} /></div>
-          <div className="watchlist-details-stock-price-percentage">stock price here</div>
-         </NavLink>
+          {/* <div className="watchlist-details-stock-price-percentage">{const data = stockOneDayChange(stock.ticker) return data["DoDPercChange"]}  {stockOneDayChange(stock.ticker)["DoDPercChange"]}</div> */}
+        </NavLink>
       ))
       }
     </div>
--- react-app/src/utils/FetchStockData.js
+++ react-app/src/utils/FetchStockData.js
@@ -10,7 +10,7 @@ export const fetchStockIntradayData = async (ticker, interval = '5min', outputsi
   return data
 }
 
-
+// fetch Daily Adjusted stock data with default outputsize of compact
 export const fetchStockDailyAdjustedData = async (ticker, outputsize = 'compact') => {
   const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${ticker}&outputsize=${outputsize}&apikey=${apiKey}`
   const response = await fetch(url);
@@ -18,9 +18,25 @@ export const fetchStockDailyAdjustedData = async (ticker, outputsize = 'compact'
   return data
 }
 
+// fetch Weekly Adjusted stock data
 export const fetchStockWeeklyAdjustedData = async (ticker) => {
   const url = `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=${ticker}&apikey=${apiKey}`
   const response = await fetch(url);
   const data = await response.json();
   return data
+}
+
+//
+export const stockOneDayChange = async (ticker) => {
+  const data = await fetchStockDailyAdjustedData(ticker);
+  // console.log("data in stockOneDayChange function", data)
+  const stockDataArr = Object.values(data["Time Series (Daily)"]);
+  const closeLatest = Number(stockDataArr[0]["4. close"])
+  const closePre = Number(stockDataArr[1]["4. close"])
+  const DoD$Change = closeLatest - closePre
+  const DoDPercChange = (closeLatest - closePre) / closePre
+  return {
+    DoD$Change,
+    DoDPercChange
+  }
 }
