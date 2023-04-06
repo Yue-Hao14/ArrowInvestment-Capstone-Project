4d94d365fd8217a6ec548d474c3099dcca7e8909
Yue Hao <105403119+Yue-Hao14@users.noreply.github.com>
2023-04-06 10:26:53 -0400

    add cash transfer API route tested

--- app/api/cash_transfer_routes.py
+++ app/api/cash_transfer_routes.py
@@ -14,6 +14,7 @@ def get_all_cash_transfers():
     """
     return [cash_transfer.to_dict() for cash_transfer in current_user.cash_transfers]
 
+
 @cash_transfer_routes.route('/', methods=['POST'])
 @login_required
 def add_cash_transfer():
