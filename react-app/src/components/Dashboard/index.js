63c812cb84175e430690e3e86dbbb172abac4113
Yue Hao <105403119+Yue-Hao14@users.noreply.github.com>
2023-04-09 18:49:44 -0400

    add buying power to dashboard page

--- react-app/src/components/Dashboard/index.js
+++ react-app/src/components/Dashboard/index.js
@@ -1,11 +1,21 @@
 import Watchlists from "../Watchlists"
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import "./Dashboard.css"
+import { useEffect } from "react";
+import { getCashTransfersThunk } from "../../store/transfer";
+import { calculateBuyingPower } from "../../utils/CalculationFunctions";
 
 function DashboardPage() {
+  const dispatch = useDispatch()
   const sessionUser = useSelector(state => state.session.user)
-  
+  const transfersArr = Object.values(useSelector(state => state.transfers))
+
+  // hydrate redux store's cash trasnfers slice
+  useEffect(()=> {
+    dispatch(getCashTransfersThunk())
+  },[])
+
   // if user has not logged in, back to landing page
   if (!sessionUser) return <Redirect to="/" />;
 
@@ -19,6 +29,12 @@ function DashboardPage() {
         />
         <h1 className="welcome-message">Welcome to Arrow Investment</h1>
       </div>
+      <div className="buying-power-container">
+        <div className="buying-power-text">Buying Power</div>
+        <div className="buying-power-amount">{calculateBuyingPower(transfersArr)}</div>
+      </div>
+
+
       <div className="dashboard-right-container">
         <Watchlists />
       </div>
---
+++ react-app/src/utils/CalculationFunctions.js
@@ -0,0 +1,9 @@
+// calculate portfolio's buying power based on all the cash transfers
+
+export const calculateBuyingPower = (transfersArr) => {
+  let buyingPower = 0
+  for (const transfer of transfersArr) {
+    buyingPower += transfer.amount
+  }
+  return buyingPower
+}
