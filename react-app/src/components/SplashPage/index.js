5667b3c844cc13ecef86df018d1ff7a5e8aadf3b
Yue Hao <105403119+Yue-Hao14@users.noreply.github.com>
2023-04-18 10:55:55 -0400

    add transition animation to landing page

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -80,12 +80,14 @@ function SplashPage() {
           <source media="(min-width:1400px)" src="https://cdn.robinhood.com/assets/generated_assets/brand/_next/static/images/retirement-hero-hq__fd36683f19bdb708d711e355fd1ed28a.mp4" type="video/mp4" />
           <source src="https://cdn.robinhood.com/assets/generated_assets/brand/_next/static/images/retirement-hero__35c1299e0ae3dcae72dbfe019a82159b.mp4" type="video/mp4" />
         </video>
-        <div className='dark-green-area-message'>Earn a 1% match.</div>
-        <div className='dark-green-area-message'>No employer necessary.</div>
-        <button className='dark-green-area-learn-more-button' onClick={() => alert("Feature coming soon...")}>Learn more.</button>
-        <div className='dark-green-area-limitation'>
-          <i className="fa-solid fa-circle-info" />
-          Limitations apply
+        <div className='bottom-darkgreen-container'>
+          <div className='dark-green-area-message'>Earn a 1% match.</div>
+          <div className='dark-green-area-message'>No employer necessary.</div>
+          <button className='dark-green-area-learn-more-button' onClick={() => alert("Feature coming soon...")}>Learn more.</button>
+          <div className='dark-green-area-limitation'>
+            <i className="fa-solid fa-circle-info" />
+            Limitations apply
+          </div>
         </div>
       </div>
 
