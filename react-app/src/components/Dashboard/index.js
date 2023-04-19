8b219bb80ae508e9383533674795e7af4da048a9
Yue Hao <105403119+Yue-Hao14@users.noreply.github.com>
2023-04-19 12:15:26 -0400

    add loading to dashboard page for testing

--- react-app/src/components/Dashboard/index.js
+++ react-app/src/components/Dashboard/index.js
@@ -9,6 +9,7 @@ import { calculateBuyingPower } from "../../utils/CalculationFunctions";
 import { getAllTransactionsThunk } from "../../store/transaction";
 import News from "./News";
 import PortfolioChart from "./PortfolioChart";
+import Loading from "../Loading";
 
 function DashboardPage() {
   const dispatch = useDispatch()
@@ -17,43 +18,51 @@ function DashboardPage() {
   const allTransactionsArr = Object.values(useSelector(state => state.transactions.allTransactions))
 
   const [newsArr, setNewsArr] = useState([])
+  const [isLoaded, setIsLoaded] = useState(false)
+
   // hydrate redux store's cash trasnfers slice as soon as user come to this page
   useEffect(() => {
     dispatch(getCashTransfersThunk())
     dispatch(getAllTransactionsThunk())
+    setIsLoaded(true)
   }, [dispatch])
 
   // if user has not logged in, back to landing page
   if (!sessionUser) return <Redirect to="/" />;
 
   return (
+    <>
+      {isLoaded ?
+        <div className="dashboard-page-container">
+          < div className="dashboard-left-container" >
+            {/* portfolio chart section */}
+            < img
+              src="https://cdn.robinhood.com/assets/generated_assets/webapp/web-platform-prefetch-sdp/member/04a63fd4f116951d91ad9b6037b42ee1.svg"
+              className="dashboard-header-pic"
+            />
 
-    <div className="dashboard-page-container">
-      <div className="dashboard-left-container">
-        {/* portfolio chart section */}
-        <img
-          src="https://cdn.robinhood.com/assets/generated_assets/webapp/web-platform-prefetch-sdp/member/04a63fd4f116951d91ad9b6037b42ee1.svg"
-          className="dashboard-header-pic"
-        />
+            {/* <PortfolioChart /> */}
+            < h1 className="welcome-message" > Welcome to Arrow Investment</h1 >
 
-        {/* <PortfolioChart /> */}
-        <h1 className="welcome-message">Welcome to Arrow Investment</h1>
+            {/* Buying power section */}
+            < div className="buying-power-container" >
+              <h3 className="buying-power-text">Buying Power</h3>
+              <div className="buying-power-amount">${calculateBuyingPower(transfersArr, allTransactionsArr).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</div>
+            </div >
 
-        {/* Buying power section */}
-        <div className="buying-power-container">
-          <h3 className="buying-power-text">Buying Power</h3>
-          <div className="buying-power-amount">${calculateBuyingPower(transfersArr, allTransactionsArr).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</div>
-        </div>
+            {/* financial news section */}
+            < News ticker="" />
 
-        {/* financial news section */}
-        <News ticker="" />
-
-        <div className="dashboard-right-container">
-          <PortfolioStock />
-          <Watchlists />
-        </div>
-      </div>
-    </div>
+            <div className="dashboard-right-container">
+              <PortfolioStock />
+              <Watchlists />
+            </div>
+          </div >
+        </div >
+        :
+        <Loading />
+      }
+    </>
   )
 }
 
---
+++ react-app/src/components/Loading/index.js
@@ -0,0 +1,12 @@
+import './Loading.css'
+
+function Loading () {
+  return (
+<div class="loading-container">
+    <div class="loading"></div>
+    <div id="loading-text">loading</div>
+</div>
+  )
+}
+
+export default Loading
