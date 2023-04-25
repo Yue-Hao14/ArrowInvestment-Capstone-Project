bba9b698d877a7ffa6c5727c30ffbadfe63d1a45
Yue Hao <105403119+Yue-Hao14@users.noreply.github.com>
2023-04-25 17:37:52 -0400

    portfolio chart calculation finally done

--- react-app/src/utils/CalculationFunctions.js
+++ react-app/src/utils/CalculationFunctions.js
@@ -70,5 +70,63 @@ export const getRandomIntInclusive = (min, max) => {
 
 // flatten stock's fetch result from Polygon Aggregates (Bars) for portfolio value calculation
 export const flattenStockFetchResult = (resultsArr) => {
-  
+  const flattenedResultObj = {};
+  resultsArr.forEach(result => {
+    const time = new Date(result.t).toLocaleString();
+    const price = result.c;
+    flattenedResultObj[time] = price;
+  })
+  return flattenedResultObj;
+}
+
+// filter timePriceArr to keep time/price pair where time/price exists for all stocks in the portfolio
+// by compare it to the benchmarkArr where hosts the least available time points based on stocks in the portfolio
+export const filterArr = (benchmarkArr, unfilteredTimePriceArr) => {
+  let filteredArr = [];
+  // loop through each stock's time/price pair
+  unfilteredTimePriceArr.forEach(timePriceObj => {
+    let tickerTimePriceObj = {};
+    // only keep the time/price pair where time exists in bechmarkArr
+    benchmarkArr.forEach(time => {
+      if (timePriceObj[time]) {
+        tickerTimePriceObj[time] = timePriceObj[time]
+      }
+    })
+    filteredArr.push(tickerTimePriceObj)
+  })
+  return filteredArr
+}
+
+
+// multiple time/price pairs in timePriceArr with # of shares in the portfolio (sharesArr)
+export const calculateTimePortfolioValue = (sharesArr, timePriceArrFiltered={}) => {
+  let portfolioTimeValueArr = [];
+  // loop through both arrays
+  for (let index = 0; index < sharesArr.length; index++) {
+    const shareQuantity = sharesArr[index];
+    const tickerTimePriceObj = timePriceArrFiltered[index];
+
+    // multiple price at each time point by shareQuantity
+    let tickerTimeValueObj = {};
+    for (let key of Object.keys(tickerTimePriceObj)) {
+      let value = tickerTimePriceObj[key] * shareQuantity;
+      tickerTimeValueObj[key] = value;
+    }
+    portfolioTimeValueArr.push(tickerTimeValueObj)
+  }
+  return portfolioTimeValueArr
+}
+
+// sum each stock value at same time point
+export const sumPortfolioValue = (portfolioTimeValueArr) => {
+  let portfolioValueArr = [];
+  for (let tickerTimeValueObj of portfolioTimeValueArr) {
+    const tickerValueArr = Object.values(tickerTimeValueObj);
+    if (portfolioValueArr.length === 0) {
+      portfolioValueArr = tickerValueArr
+    } else {
+      portfolioValueArr = sumNumSameIndex(tickerValueArr,portfolioValueArr)
+    }
+  }
+  return portfolioValueArr;
 }
